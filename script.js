4f5036e2150e8d00fa8f79b1117efd1ffa29de72
wira-pixel <sayaryant@gmail.com>
2025-08-11 12:46:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // URL WEB APP YANG SUDAH DIPERBARUI
     const GOOGLE_APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxqom1sVW_QlqMBY93g1E37B9PovVC2XNLD2ec30yhrDV6EZG-6fImBxFhz9n0cRYY/exec";
-
+    
     const inspectionBody = document.getElementById('inspection-body');
     const addRowBtn = document.getElementById('add-row');
     const savePdfBtn = document.getElementById('save-pdf');
@@ -149,3 +149,4 @@ function previewImage(input) {
         reader.readAsDataURL(input.files[0]);
     }
 }
+
