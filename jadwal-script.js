8234c2d6f5031350b232471c17d2ebbe9fcdd8dd
wira-pixel <sayaryant@gmail.com>
2025-08-24 12:08:55 +0700

    Update jadwal-script.js

--- jadwal-script.js
+++ jadwal-script.js
@@ -1,5 +1,5 @@
 // Ganti dengan URL Cloudflare Worker kamu
-const WORKER_URL = ""; 
+const WORKER_URL = "https://delicate-union-ad99.sayaryant.workers.dev/"; 
 
 // Form submit
 document.getElementById("jadwalForm").addEventListener("submit", async (e) => {
